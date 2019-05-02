0af23490199c85b6ddb443a14aa3c108d669b87d
MD. JUEL HOSSAIN <juel444@gmail.com>
2019-05-03 01:18:28 +0600

    Alarm Manager added

--- app/src/main/java/com/example/locationtracker/MainActivity.java
+++ app/src/main/java/com/example/locationtracker/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.locationtracker;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
@@ -7,6 +10,8 @@ import android.os.PowerManager;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,23 +21,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // disable doze mood
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            Intent intent = new Intent();
-            String packageName = getPackageName();
-            PowerManager pm = (PowerManager) getSystemService(POWER_SERVICE);
-            if (!pm.isIgnoringBatteryOptimizations(packageName)) {
-                intent.setAction(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS);
-                intent.setData(Uri.parse("package:" + packageName));
-                startActivity(intent);
-            }
-        }
+//        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+//            Intent intent = new Intent();
+//            String packageName = getPackageName();
+//            PowerManager pm = (PowerManager) getSystemService(POWER_SERVICE);
+//            if (!pm.isIgnoringBatteryOptimizations(packageName)) {
+//                intent.setAction(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS);
+//                intent.setData(Uri.parse("package:" + packageName));
+//                startActivity(intent);
+//            }
+//        }
 
-        Intent myServiceIntent = new Intent(this, MyService.class);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            startForegroundService(myServiceIntent);
-        }else {
-            startService(myServiceIntent);
-        }
+//        Intent myServiceIntent = new Intent(this, MyService.class);
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//            startForegroundService(myServiceIntent);
+//        }else {
+//            startService(myServiceIntent);
+//        }
 
+        startAlarm();
     }
+
+   private void startAlarm() {
+       AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+       Intent intent = new Intent(this, AlarmReceiver.class);
+       PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 1, intent, 0 );
+       Calendar calendar = Calendar.getInstance();
+//       calendar.add(Calendar.SECOND, 15);
+       long time = System.currentTimeMillis();
+       alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), 15000, pendingIntent);
+   }
 }
---
+++ app/src/main/java/com/example/locationtracker/NotificationHelper.java
@@ -0,0 +1,45 @@
+package com.example.locationtracker;
+
+import android.annotation.TargetApi;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+
+public class NotificationHelper extends ContextWrapper {
+    public static final String channelID = "notChannel";
+    public static final String channelName = "Foreground Channel";
+
+    private NotificationManager notificationManager;
+
+    public NotificationHelper(Context base) {
+        super(base);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createChannel();
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.O)
+    private void createChannel() {
+        NotificationChannel notificationChannel = new NotificationChannel(channelID, channelName, NotificationManager.IMPORTANCE_HIGH);
+        getManager().createNotificationChannel(notificationChannel);
+    }
+
+    public NotificationManager getManager() {
+        if (notificationManager == null) {
+            notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        }
+
+        return notificationManager;
+    }
+
+    public NotificationCompat.Builder getChannelNotification() {
+        return new NotificationCompat.Builder(getApplicationContext(), channelID)
+                .setContentTitle("Ship Tracking")
+                .setContentText("Location Sent to Server...")
+                .setSmallIcon(R.drawable.ic_sync_black_24dp);
+    }
+}
